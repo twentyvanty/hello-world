07c00ffbac14355154a245aad190c6cbd814468e
twentyvanty <lovelyzkim4@gmail.com>
2025-04-11 13:20:43 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,29 @@
-public class whatIsEvenThis123 {
+public class ArraySorter {
 
-public static void doStuffLol(int[] bOb)
-{ for(int i=0;i<bOb.length-2;i++)
-{ for(int j=0;j<bOb.length-i-1;j++)
-{ if(bOb[j]<bOb[j+1]) // should be > for ascending, bug!
-{
-int tmp= bOb[j];
-bOb[j]=bOb[j+1];
-bOb[j+1]=tmp;
-}
-} } }
+  /**
+   * Sorts the given array in ascending order using bubble sort.
+   *
+   * @param array the array to be sorted
+   */
+  public static void sortAscending(int[] array) {
+    for (int i = 0; i < array.length - 1; i++) {
+      for (int j = 0; j < array.length - i - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          int temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
+  }
 
-public static void main(String args[])
-{
-int xXx[]={4,2,7,1,9,0};
-doStuffLol(xXx);
-for(int a=0;a<=xXx.length;a++) // bug: should be a<xXx.length
-System.out.print(xXx[a]+" ");
-}
+  public static void main(String[] args) {
+    int[] numbers = {4, 2, 7, 1, 9, 0};
+    sortAscending(numbers);
+
+    System.out.println("Sorted array:");
+    for (int number : numbers) {
+      System.out.print(number + " ");
+    }
+  }
 }
